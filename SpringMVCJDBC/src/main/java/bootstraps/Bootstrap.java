1dac2c41c632b1d0fc7b43f7f35f3cc3f65b32f1
darapenhchet <darapenhchet@gmail.com>
2016-04-23 14:28:32 +0700

    SpringMVCJDBC

---
+++ SpringMVCJDBC/src/main/java/bootstraps/Bootstrap.java
@@ -0,0 +1,27 @@
+package bootstraps;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import configurations.ServletConfiguration;
+
+public class Bootstrap implements WebApplicationInitializer{
+	
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+			
+		AnnotationConfigWebApplicationContext servletContext = new AnnotationConfigWebApplicationContext();
+		servletContext.register(ServletConfiguration.class);
+		
+		ServletRegistration.Dynamic dispatcher = 
+				container.addServlet("springServlet", new DispatcherServlet(servletContext));
+		dispatcher.addMapping("/");
+		
+	}
+	
+}
